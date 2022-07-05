e330844163f69beeb6352bdcbfeec8fe7587c176
cwangg897 <dhkdrb897@gmail.com>
2022-07-05 18:20:44 +0900

    [#3] 프로젝트 세팅

---
+++ src/main/java/com/flab/homedeli/HomedeliApplication.java
@@ -0,0 +1,13 @@
+package com.flab.homedeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomedeliApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/flab/homedeli/HomedeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.homedeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
