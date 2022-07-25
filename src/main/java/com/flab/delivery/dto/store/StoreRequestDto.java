841c731e033b6a645f3b462679b5fe97f9de959f
cwangg897 <dhkdrb897@gmail.com>
2022-07-25 22:15:10 +0900

    Fix : notempty -> notnull

--- src/main/java/com/flab/delivery/dto/store/StoreRequestDto.java
+++ src/main/java/com/flab/delivery/dto/store/StoreRequestDto.java
@@ -12,10 +12,10 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class StoreRequestDto {
 
-    @NotBlank(message = "주소 정보가 올바르지 않습니다")
+    @NotNull(message = "주소 정보가 올바르지 않습니다")
     private Long addressId;
 
-    @NotBlank(message = "카테고리 정보가 올바르지 않습니다")
+    @NotNull(message = "카테고리 정보가 올바르지 않습니다")
     private Long categoryId;
 
     // ownerId 없음 왜냐 세션에 있기때문에 안받습니다
@@ -41,6 +41,6 @@ public class StoreRequestDto {
     @NotBlank(message = "마감 시간 정보가 올바르지 않습니다")
     private String endTime;
 
-    @NotBlank(message = "최소 주문 가격 정보가 올바르지 않습니다")
+    @NotNull(message = "최소 주문 가격 정보가 올바르지 않습니다")
     private Long minPrice;
 }
