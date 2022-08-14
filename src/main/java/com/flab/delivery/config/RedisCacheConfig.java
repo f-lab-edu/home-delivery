8b2c6a4bc8f0a6e17a722911dcab25a6f560bb01
cwangg897 <dhkdrb897@gmail.com>
2022-08-14 15:40:46 +0900

    Refactor : 레디스 세션 캐시 저장소 분리

---
+++ src/main/java/com/flab/delivery/config/RedisCacheConfig.java
@@ -0,0 +1,56 @@
+package com.flab.delivery.config;
+
+import com.flab.delivery.utils.CacheConstants;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.flab.delivery.utils.CacheConstants.*;
+
+@EnableCaching
+@Configuration
+public class RedisCacheConfig {
+    @Value("${spring.redis.cache.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.cache.port}")
+    private int redisPort;
+
+    @Bean("redisCacheConnectionFactory")
+    public RedisConnectionFactory redisCacheConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(@Qualifier("redisCacheConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(DEFAULT_EXPIRE_TIME_MIN);
+
+
+        Map<String, RedisCacheConfiguration> configurations = new HashMap<>();
+        configurations.put(CATEGORY, redisCacheConfiguration.entryTtl(CATEGORY_EXPIRE_TIME_MIN));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .withInitialCacheConfigurations(configurations)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
