be1812301eb1771903130707c172c6b118523967
sukeun <busan-sy@naver.com>
2022-09-01 22:29:30 +0900

    Add : Dockerfile
    Refactor : Data.sql

--- src/test/resources/data/data.sql
+++ src/test/resources/data/data.sql
@@ -1,23 +1,9 @@
 CREATE TABLE IF NOT EXISTS `address`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `town_name`
-    varchar
-(
-    50
-) NOT NULL,
-    PRIMARY KEY
-(
-    `id`
-),
-    UNIQUE KEY `town_name`
-(
-    `town_name`
-)
+    `id`        bigint      NOT NULL AUTO_INCREMENT,
+    `town_name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `town_name` (`town_name`)
     ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `address`
@@ -25,64 +11,29 @@ VALUES (3, '상수동'),
        (2, '영통동'),
        (1, '운암동');
 
-CREATE TABLE IF NOT EXISTS `category`
+CREATE TABLE  IF NOT EXISTS `category`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `name`
-    varchar
-(
-    50
-) NOT NULL,
-    PRIMARY KEY
-(
-    `id`
-),
-    UNIQUE KEY `name`
-(
-    `name`
-)
+    `id`   bigint      NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`)
     ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `category`
 VALUES (1, '족발'),
        (2, '치킨');
 
-CREATE TABLE IF NOT EXISTS `user`
+CREATE TABLE  IF NOT EXISTS  `user`
 (
-    `id` varchar
-(
-    50
-) NOT NULL,
-    `email` varchar
-(
-    50
-) NOT NULL,
-    `password` varchar
-(
-    255
-) NOT NULL,
-    `name` varchar
-(
-    30
-) NOT NULL,
-    `phone_number` varchar
-(
-    30
-) NOT NULL,
-    `type` varchar
-(
-    20
-) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    `id`
-)
+    `id`           varchar(50)  NOT NULL,
+    `email`        varchar(50)  NOT NULL,
+    `password`     varchar(255) NOT NULL,
+    `name`         varchar(30)  NOT NULL,
+    `phone_number` varchar(30)  NOT NULL,
+    `type`         varchar(20)  NOT NULL,
+    `created_at`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `user`
@@ -95,60 +46,21 @@ VALUES ('rider1', 'test@mail.com', '$2a$10$Uj.pN4fw2AZ1n5eCCtKFiutvieNbHpAhiOdQc
        ('user2', 'user2@naver.com', '$2a$10$Uj.pN4fw2AZ1n5eCCtKFiutvieNbHpAhiOdQc4yW/klpm27F6ex32', '유저1',
         '010-1111-1123', 'OWNER', '2022-07-20 12:45:01', '2022-08-18 09:19:12');
 
-CREATE TABLE IF NOT EXISTS `user_address`
+CREATE TABLE  IF NOT EXISTS `user_address`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `user_id`
-    varchar
-(
-    50
-) NOT NULL,
-    `address_id` bigint NOT NULL,
-    `detail_address` varchar
-(
-    50
-) NOT NULL,
-    `alias` varchar
-(
-    50
-) NOT NULL,
-    `selected` tinyint NOT NULL DEFAULT '0',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `user_id`
-(
-    `user_id`
-),
-    KEY `address_id`
-(
-    `address_id`
-),
-    CONSTRAINT `user_address_ibfk_1` FOREIGN KEY
-(
-    `user_id`
-) REFERENCES `user`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE,
-    CONSTRAINT `user_address_ibfk_2` FOREIGN KEY
-(
-    `address_id`
-) REFERENCES `address`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE
+    `id`             bigint      NOT NULL AUTO_INCREMENT,
+    `user_id`        varchar(50) NOT NULL,
+    `address_id`     bigint      NOT NULL,
+    `detail_address` varchar(50) NOT NULL,
+    `alias`          varchar(50) NOT NULL,
+    `selected`       tinyint     NOT NULL DEFAULT '0',
+    `created_at`     timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`    timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY              `user_id` (`user_id`),
+    KEY              `address_id` (`address_id`),
+    CONSTRAINT `user_address_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `user_address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
     ) ENGINE=InnoDB AUTO_INCREMENT=337 DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `user_address`
@@ -167,99 +79,29 @@ VALUES (15, 'user1', 1, '15번길 13로', '15번길 13로', 0, '2022-07-25 10:56
        (68, 'user1', 1, '15번길 13로 101동 103호', '우리집', 0, '2022-07-28 11:49:39', '2022-07-28 11:49:39'),
        (69, 'user1', 1, '15번길 13로 101동 103호', '우리집', 0, '2022-07-28 11:51:50', '2022-07-28 11:51:50');
 
-CREATE TABLE IF NOT EXISTS `store`
+CREATE TABLE  IF NOT EXISTS `store`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `address_id`
-    bigint
-    NOT
-    NULL,
-    `user_id`
-    varchar
-(
-    50
-) NOT NULL,
-    `category_id` bigint NOT NULL,
-    `detail_address` varchar
-(
-    50
-) NOT NULL,
-    `name` varchar
-(
-    50
-) NOT NULL,
-    `phone_number` varchar
-(
-    50
-) NOT NULL,
-    `info` varchar
-(
-    255
-) NOT NULL,
-    `status` varchar
-(
-    20
-) NOT NULL DEFAULT 'CLOSED',
-    `open_time` varchar
-(
-    50
-) NOT NULL,
-    `end_time` varchar
-(
-    50
-) NOT NULL,
-    `min_price` bigint NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `user_id`
-(
-    `user_id`
-),
-    KEY `category_id`
-(
-    `category_id`
-),
-    KEY `idx_store_address_id_category_id_status`
-(
-    `address_id`,
-    `category_id`,
-    `status`
-) /*!80000 INVISIBLE */,
-    CONSTRAINT `store_ibfk_1` FOREIGN KEY
-(
-    `address_id`
-) REFERENCES `address`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE,
-    CONSTRAINT `store_ibfk_2` FOREIGN KEY
-(
-    `user_id`
-) REFERENCES `user`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE,
-    CONSTRAINT `store_ibfk_3` FOREIGN KEY
-(
-    `category_id`
-) REFERENCES `category`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE
+    `id`             bigint       NOT NULL AUTO_INCREMENT,
+    `address_id`     bigint       NOT NULL,
+    `user_id`        varchar(50)  NOT NULL,
+    `category_id`    bigint       NOT NULL,
+    `detail_address` varchar(50)  NOT NULL,
+    `name`           varchar(50)  NOT NULL,
+    `phone_number`   varchar(50)  NOT NULL,
+    `info`           varchar(255) NOT NULL,
+    `status`         varchar(20)  NOT NULL DEFAULT 'CLOSED',
+    `open_time`      varchar(50)  NOT NULL,
+    `end_time`       varchar(50)  NOT NULL,
+    `min_price`      bigint       NOT NULL,
+    `created_at`     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY              `user_id` (`user_id`),
+    KEY              `category_id` (`category_id`),
+    KEY              `idx_store_address_id_category_id_status` (`address_id`,`category_id`,`status`) /*!80000 INVISIBLE */,
+    CONSTRAINT `store_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `store_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `store_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
     ) ENGINE=InnoDB AUTO_INCREMENT=1557380 DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `store`
@@ -272,46 +114,18 @@ VALUES (1, 1, 'user2', 1, '매장상세주소 101호', 'BBQ치킨', '010-1111-11
        (4, 2, 'user2', 1, '매장상세주소 104호', 'BBQ치킨 4호점', '010-1111-1111', '치킨집입니다', 'CLOSED', '11:00', '23:00', 18000,
         '2022-07-16 04:35:47', '2022-07-26 11:30:30');
 
-CREATE TABLE IF NOT EXISTS `menu_group`
+CREATE TABLE  IF NOT EXISTS `menu_group`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `store_id`
-    bigint
-    NOT
-    NULL,
-    `name`
-    varchar
-(
-    50
-) NOT NULL,
-    `info` varchar
-(
-    255
-) NOT NULL,
-    `priority` int NOT NULL DEFAULT '1',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `store_id`
-(
-    `store_id`
-),
-    CONSTRAINT `menu_group_ibfk_1` FOREIGN KEY
-(
-    `store_id`
-) REFERENCES `store`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE
+    `id`          bigint       NOT NULL AUTO_INCREMENT,
+    `store_id`    bigint       NOT NULL,
+    `name`        varchar(50)  NOT NULL,
+    `info`        varchar(255) NOT NULL,
+    `priority`    int          NOT NULL DEFAULT '1',
+    `created_at`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY           `store_id` (`store_id`),
+    CONSTRAINT `menu_group_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
     ) ENGINE=InnoDB AUTO_INCREMENT=209 DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `menu_group`
@@ -330,49 +144,18 @@ VALUES (1, 1, '단품', '치킨 단품입니다', 1, '2022-08-02 10:36:47', '202
 
 CREATE TABLE IF NOT EXISTS `menu`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `menu_group_id`
-    bigint
-    NOT
-    NULL,
-    `name`
-    varchar
-(
-    50
-) NOT NULL,
-    `info` varchar
-(
-    255
-) NOT NULL,
-    `price` int NOT NULL,
-    `status` varchar
-(
-    20
-) NOT NULL DEFAULT 'ONSALE',
-    `priority` int NOT NULL DEFAULT '0',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `menu_group_id`
-(
-    `menu_group_id`
-),
-    CONSTRAINT `menu_ibfk_1` FOREIGN KEY
-(
-    `menu_group_id`
-) REFERENCES `menu_group`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE
+    `id`            bigint       NOT NULL AUTO_INCREMENT,
+    `menu_group_id` bigint       NOT NULL,
+    `name`          varchar(50)  NOT NULL,
+    `info`          varchar(255) NOT NULL,
+    `price`         int          NOT NULL,
+    `status`        varchar(20)  NOT NULL DEFAULT 'ONSALE',
+    `priority`      int          NOT NULL DEFAULT '0',
+    `created_at`    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY             `menu_group_id` (`menu_group_id`),
+    CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`menu_group_id`) REFERENCES `menu_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
     ) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=utf8mb3;
 
 
@@ -395,40 +178,16 @@ VALUES (1, 1, '후라이드 치킨', '빵가루', 18000, 'ONSALE', 0, '2022-08-0
        (17, 8, '포토주스', '포도주스 입니다', 4000, 'ONSALE', 2, '2022-08-11 05:39:12', '2022-08-11 09:31:48');
 
 
-CREATE TABLE IF NOT EXISTS `options`
+CREATE TABLE  IF NOT EXISTS `options`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `menu_id`
-    bigint
-    NOT
-    NULL,
-    `name`
-    varchar
-(
-    50
-) NOT NULL,
-    `price` int NOT NULL DEFAULT '0',
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `menu_id`
-(
-    `menu_id`
-),
-    CONSTRAINT `options_ibfk_1` FOREIGN KEY
-(
-    `menu_id`
-) REFERENCES `menu`
-(
-    `id`
-) ON DELETE CASCADE
-  ON UPDATE CASCADE
-    ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
+    `id`      bigint      NOT NULL AUTO_INCREMENT,
+    `menu_id` bigint      NOT NULL,
+    `name`    varchar(50) NOT NULL,
+    `price`   int         NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    KEY       `menu_id` (`menu_id`),
+    CONSTRAINT `options_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    ) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8mb3;
 
 
 INSERT INTO `options`
@@ -451,105 +210,37 @@ VALUES (3, 1, '치즈볼 추가', 5000),
        (19, 17, '휘핑크림 추가', 500),
        (20, 17, '케이크 추가', 5000);
 
-SET
-sql_mode = '';
+SET sql_mode = '';
 
-CREATE TABLE IF NOT EXISTS `orders`
+CREATE TABLE  IF NOT EXISTS `orders`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `user_id`
-    varchar
-(
-    50
-) NOT NULL,
-    `store_id` bigint DEFAULT NULL,
-    `rider_id` varchar
-(
-    45
-) DEFAULT NULL,
-    `order_price` int NOT NULL,
-    `status` varchar
-(
-    40
-) NOT NULL,
-    `start_time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
-    `end_time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `history` json NOT NULL,
-    `delivery_address` varchar
-(
-    50
-) NOT NULL,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `orders_ibfk_1`
-(
-    `user_id`
-),
-    KEY `orders_ibfk_2`
-(
-    `store_id`
-),
-    CONSTRAINT `orders_ibfk_1` FOREIGN KEY
-(
-    `user_id`
-) REFERENCES `user`
-(
-    `id`
-)
-                                                               ON DELETE CASCADE
-                                                               ON UPDATE CASCADE,
-    CONSTRAINT `orders_ibfk_2` FOREIGN KEY
-(
-    `store_id`
-) REFERENCES `store`
-(
-    `id`
-)
-                                                               ON DELETE SET NULL
+    `id`               bigint      NOT NULL AUTO_INCREMENT,
+    `user_id`          varchar(50) NOT NULL,
+    `store_id`         bigint               DEFAULT NULL,
+    `rider_id`         varchar(45)          DEFAULT NULL,
+    `order_price`      int         NOT NULL,
+    `status`           varchar(40) NOT NULL,
+    `start_time`       timestamp NULL DEFAULT '0000-00-00 00:00:00',
+    `end_time`         timestamp NULL DEFAULT '0000-00-00 00:00:00',
+    `created_at`       timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_at`      timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `history`          json        NOT NULL,
+    `delivery_address` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY                `orders_ibfk_1` (`user_id`),
+    KEY                `orders_ibfk_2` (`store_id`),
+    CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE SET NULL
     ) ENGINE=InnoDB AUTO_INCREMENT=5774 DEFAULT CHARSET=utf8mb3;
 
 
-CREATE TABLE IF NOT EXISTS `pay`
+CREATE TABLE  IF NOT EXISTS `pay`
 (
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `orders_id`
-    bigint
-    NOT
-    NULL,
-    `type`
-    varchar
-(
-    40
-) NOT NULL,
-    `status` varchar
-(
-    40
-) NOT NULL,
-    PRIMARY KEY
-(
-    `id`
-),
-    KEY `orders`
-(
-    `orders_id`
-),
-    CONSTRAINT `pay_ibfk_1` FOREIGN KEY
-(
-    `orders_id`
-) REFERENCES `orders`
-(
-    `id`
-) ON DELETE CASCADE
+    `id`        bigint      NOT NULL AUTO_INCREMENT,
+    `orders_id` bigint      NOT NULL,
+    `type`      varchar(40) NOT NULL,
+    `status`    varchar(40) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY         `orders` (`orders_id`),
+    CONSTRAINT `pay_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE
     ) ENGINE=InnoDB AUTO_INCREMENT=423 DEFAULT CHARSET=utf8mb3;
